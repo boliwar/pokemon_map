94308531eabfef65635daed185e6ff53e7ff5b35
boliwar <boliwar@gmail.com>
2022-11-29 10:12:04 +0200

    egister Pokemon model on admin UI.

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,3 +1,5 @@
-from django.db import models  # noqa F401
+from django.db import models
 
-# your models here
+class Pokemon(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=200)
