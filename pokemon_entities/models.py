2c8c7cacf80192e83a919c9d3ce7ba333fc59261
boliwar <boliwar@gmail.com>
2022-12-01 17:06:26 +0200

    регистр букв

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -23,11 +23,11 @@ class PokemonEntity(models.Model):
     lon = models.FloatField('Долгота', )
     appeared_at = models.DateTimeField('Начало видимости', null=True)
     disappeared_at = models.DateTimeField('Окончание видимости', null=True)
-    Level = models.IntegerField('Уровень', default=1, blank=True)
-    Health = models.IntegerField('Здоровье', default=1, blank=True)
-    Attack = models.IntegerField('Атака', default=1, blank=True)
-    Protection = models.IntegerField('Защита', default=1, blank=True)
-    Endurance = models.IntegerField('Выносливость', default=1, blank=True)
+    level = models.IntegerField('Уровень', default=1, blank=True)
+    health = models.IntegerField('Здоровье', default=1, blank=True)
+    attack = models.IntegerField('Атака', default=1, blank=True)
+    protection = models.IntegerField('Защита', default=1, blank=True)
+    endurance = models.IntegerField('Выносливость', default=1, blank=True)
 
     def __str__(self):
         return f'({self.lat}, {self.lon})[{self.appeared_at} - {self.disappeared_at}]'
