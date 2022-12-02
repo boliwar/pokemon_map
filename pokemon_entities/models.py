198e3b3d31e67602dbe123e6a962937e2f695627
boliwar <boliwar@gmail.com>
2022-12-02 21:52:54 +0200

    плохое дефолтное значение

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -23,11 +23,11 @@ class PokemonEntity(models.Model):
     lon = models.FloatField('Долгота', )
     appeared_at = models.DateTimeField('Начало видимости', null=True)
     disappeared_at = models.DateTimeField('Окончание видимости', null=True)
-    level = models.IntegerField('Уровень', default=1, blank=True)
-    health = models.IntegerField('Здоровье', default=1, blank=True)
-    attack = models.IntegerField('Атака', default=1, blank=True)
-    protection = models.IntegerField('Защита', default=1, blank=True)
-    endurance = models.IntegerField('Выносливость', default=1, blank=True)
+    level = models.IntegerField('Уровень', blank=True)
+    health = models.IntegerField('Здоровье', blank=True)
+    attack = models.IntegerField('Атака', blank=True)
+    protection = models.IntegerField('Защита', blank=True)
+    endurance = models.IntegerField('Выносливость', blank=True)
 
     def __str__(self):
         return f'({self.lat}, {self.lon})[{self.appeared_at} - {self.disappeared_at}]'
